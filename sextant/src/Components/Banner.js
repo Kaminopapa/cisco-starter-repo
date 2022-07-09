70d9df0e3a11c669f302602f8d7e20fde81988ba
Luis Hung <luishung1996@gmail.com>
2022-07-08 19:30:49 -0700

    Module 2

---
+++ sextant/src/Components/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+const Banner = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#home">Sextant</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ sextant/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "./Banner";
+import VisualizeData from "./VisualizeData";
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <VisualizeData />
+    </div>
+  );
+};
+
+export default Home;
