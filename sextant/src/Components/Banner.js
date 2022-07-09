b15372751de24262b31659a52d7954fd662655ff
Luis Hung <luishung1996@gmail.com>
2022-07-08 19:54:59 -0700

    Module2: modified style

--- sextant/src/Components/Banner.js
+++ sextant/src/Components/Banner.js
@@ -2,13 +2,13 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import NavDropdown from "react-bootstrap/NavDropdown";
-const Banner = () => {
+
+const Banner = (props) => {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#home">Sextant</Navbar.Brand>
+          <Navbar.Brand href="#home">{props.bannerText}</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto">
--- sextant/src/Components/Home.js
+++ sextant/src/Components/Home.js
@@ -4,8 +4,12 @@ import VisualizeData from "./VisualizeData";
 const Home = () => {
   return (
     <div>
-      <Banner />
-      <VisualizeData />
+      <Banner bannerText="Sextant" />
+      <div className="Cards">
+        <VisualizeData title="Show case1" content="I am the 1st show case" />
+        <VisualizeData title="Show case2" content="I am the 2nd show case" />
+        <VisualizeData title="Show case3" content="I am the 3rd show case" />
+      </div>
     </div>
   );
 };
